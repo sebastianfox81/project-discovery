cb1e8682ccebf4117b66ed1583f3a80f2324ab04
Sebastian <sebastianfox81@gmail.com>
2022-04-25 13:50:02 -0400

    Sidebar component skeleton complete

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   <Wrapper>
     <h5>
       &copy; {new Date().getFullYear()}
-    <span>Discovery</span>
+    <span> Discovery</span>
     </h5>
     <h5>All Rights Reserved</h5>
   </Wrapper>
