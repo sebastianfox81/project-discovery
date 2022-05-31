0a3b8a6d899785730c61febbbedc557a172ef345
Sebastian <sebastianfox81@gmail.com>
2022-05-31 01:06:49 -0400

    CartPage complete

--- client/src/context/cart_context.js
+++ client/src/context/cart_context.js
@@ -8,11 +8,20 @@ import {
   COUNT_CART_TOTALS,
 } from '../actions'
 
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart')
+  if(cart) {
+    return JSON.parse(localStorage.getItem('cart'))
+  } else {
+    return []
+  }
+}
+
 const initialState = {
-  cart: [],
+  cart: getLocalStorage(),
   total_items: 0,
   total_amount: 0,
-  shipping_fee: 534
+  shipping_fee: 5.34
 
 }
 
@@ -25,19 +34,23 @@ export const CartProvider = ({ children }) => {
   // add to cart
   const addToCart = (id, amount, product) => {
     dispatch({ type: ADD_TO_CART, payload: { id, amount, product}})
-    console.log(id)
   }
   const removeItem = (id) => {
-
+    dispatch({ type: REMOVE_CART_ITEM, payload: id})
   }
 
-  const toggleAmount = (id, value) => {
-
+  const toggleAmount = (id, type) => {
+    dispatch({ type: TOGGLE_CART_ITEM_AMOUNT, payload: { id, type }})
   }
 
   const clearCart = () => {
-
+    dispatch({ type: CLEAR_CART})
   }
+
+  useEffect(() => {
+    dispatch({ type: COUNT_CART_TOTALS})
+    localStorage.setItem('cart', JSON.stringify(state.cart))
+  }, [state.cart])
   return (
     <CartContext.Provider value={{...state, addToCart, removeItem, toggleAmount, clearCart}}>{children}</CartContext.Provider>
   )
--- client/src/reducers/cart_reducer.js
+++ client/src/reducers/cart_reducer.js
@@ -41,7 +41,64 @@ const cart_reducer = (state, action) => {
       }
     }
   }
-
+  if (action.type === REMOVE_CART_ITEM) {
+    let tempCart = state.cart.filter((item) => {
+      return item.id !== action.payload
+    })
+    return {
+      ...state,
+      cart: tempCart
+    }
+  }
+  if (action.type === CLEAR_CART) {
+    return {
+      ...state,
+      cart: []
+    }
+  }
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, type } = action.payload
+    const tempCart = state.cart.map((item) => {
+      if (item.id === id) {
+        if (type === 'inc') {
+          let newAmount = item.amount + 1
+          if (newAmount > item.max) {
+            newAmount = item.max
+          }
+          return {...item, amount: newAmount}
+        }
+        if (type === 'dec') {
+          let newAmount = item.amount - 1
+          if (newAmount < 1) {
+            newAmount = 1
+          }
+          return {...item, amount: newAmount}
+        }
+      } else {
+        return item
+      }
+    })
+    return {
+      ...state,
+      cart: tempCart
+    }
+  }
+  if (action.type === COUNT_CART_TOTALS) {
+    const totals = state.cart.reduce((acc, curr) => {
+      const { amount, price } = curr
+      acc.total_items += amount
+      acc.total_amount += amount * price
+      return acc
+    }, {
+      total_items: 0,
+      total_amount: 0
+    })
+    return {
+      ...state,
+      total_items: totals.total_items,
+      total_amount: totals.total_amount
+    }
+  }
   return state
   throw new Error(`No Matching "${action.type}" - action type`)
 }
