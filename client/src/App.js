40e8afd504be40087b2ce5bba64b2c859455d17f
Sebastian <sebastianfox81@gmail.com>
2022-06-01 17:05:28 -0400

    Implemented private route component

--- client/src/App.js
+++ client/src/App.js
@@ -31,9 +31,9 @@ function App() {
         <Route exact path='/products'>
           <Products />
         </Route>
-        <Route exact path='/checkout'>
+        <PrivateRoute exact path='/checkout'>
           <Checkout />
-        </Route>
+        </PrivateRoute>
         <Route exact path='/products/:guitId' children={<SingleProduct/>} />
         <Route path='*'>
           <Error />
